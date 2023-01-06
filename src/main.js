cedc0ebaec045945374a2a772d91e6c711ce50b3
pkqLin <319337902@qq.com>
2023-01-06 08:26:03 +0800

    升级路由

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store/index'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './css/global.css';
 import request from "@/utils/request";
 
-Vue.use(ElementUI,{size: 'small'});
+Vue.use(ElementUI, { size: 'small' });
 Vue.config.productionTip = false
 
-Vue.prototype.request=request
+Vue.prototype.request = request
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount('#app')
